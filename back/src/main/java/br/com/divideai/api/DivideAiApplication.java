2e6d545f8da18329ce9ab2c145ac1150502e66f2
manuelaaime <aimemanu28@gmail.com>
2026-02-19 17:46:01 -0300

    chore: Adicionando API inicial

---
+++ back/src/main/java/br/com/divideai/api/DivideAiApplication.java
@@ -0,0 +1,13 @@
+package br.com.divideai.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivideAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DivideAiApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/br/com/divideai/api/DivideAiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.divideai.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DivideAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
