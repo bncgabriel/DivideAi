ee2b3e7ab064f720d6431d4b3ff8d9d419ac5c44
bncgabriel <gabrielbenicioo@gmail.com>
2026-02-23 16:17:06 -0300

    feat(front/routes): configurar rotas das páginas

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,34 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
+import DashboardPage from './pages/DashboardPage'
+import GroupDetailsPage from './pages/GroupDetailsPage'
+import LoginPage from './pages/LoginPage'
+import MyGroupsPage from './pages/MyGroupsPage'
+import NewExpensePage from './pages/NewExpensePage'
+import SettingsPage from './pages/SettingsPage'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="/groups" element={<MyGroupsPage />} />
+      <Route path="/groups/:groupId" element={<GroupDetailsPage />} />
+      <Route path="/groups/:groupId/expense/new" element={<NewExpensePage />} />
+      <Route path="/settings" element={<SettingsPage />} />
+      <Route path="*" element={<Navigate to="/login" replace />} />
+    </Routes>
   )
 }
 
